8df9ad8d30b960e78fc0fadd9fc5f766c51fdb3b
annapaulabarros <annapaula.barrosdasilva@gmail.com>
2024-07-28 20:45:14 -0300

    Desafio banco digital

---
+++ Desafio Banco digital/Desafio banco digital/src/Conta.java
@@ -0,0 +1,53 @@
+public abstract class Conta implements Transacoes {
+    private static int SEQUENCIAL = 1;
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = 1;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+        this.saldo = 0.0;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agência: %d", this.agencia));
+        System.out.println(String.format("Número: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
