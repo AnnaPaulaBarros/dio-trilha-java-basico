d0e8eac6ccdc35d5ecf66195f45421e8c296d1c3
annapaulabarros <annapaula.barrosdasilva@gmail.com>
2024-07-28 14:28:56 -0300

    desafio conta

--- conta-Banco/src/ContaTerminal.java
+++ conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o nosso usuário
+        System.out.println("Bem-vindo! Vamos criar uma conta.");
+
+        // Obter os valores digitados no terminal
+        System.out.print("Digite o nome do titular da conta: ");
+        String nomeTitular = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Digite o saldo inicial da conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Exibir mensagem da conta criada
+        System.out.println("\nConta criada com sucesso:");
+        System.out.println("Titular: " + nomeTitular);
+        System.out.println("Número da Conta: " + numeroConta);
+        System.out.println("Saldo: R$ " + saldo);
+
+        scanner.close();
     }
 }
